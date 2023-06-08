ad497ec4c1958422a972b258be5aaf56d777a588
Nicolas Melo <jaimenicolasmelo@gmail.com>
2023-06-08 18:08:47 -0500

    Estilos y formularios

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+
 app.listen(3000, () => {
     console.log("Servidor corriendo");
 })
