fd47c3f9a5f0bcb726ca59e0ed49212e046471aa
Nicolas Melo <jaimenicolasmelo@gmail.com>
2023-06-08 13:36:32 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+})
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo");
+})
